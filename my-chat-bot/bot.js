4bfbbb79d996348250a83bb1da3f14e3434c2c49
elder oliveira <elder.oliveira@globalhitss.com.br>
2019-02-21 12:02:32 -0300

    fix contexto

--- my-chat-bot/bot.js
+++ my-chat-bot/bot.js
@@ -9,15 +9,20 @@ class MyBot {
      *
      * @param {TurnContext} on turn context object.
      */
+    constructor() {
+        let contexto = {};
+    };
     async onTurn(turnContext) {
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         if (turnContext.activity.type === ActivityTypes.Message) {
             await axios.post('https://orchestrator-skype-bot.herokuapp.com/orchestrator', {
-                text: turnContext._activity.text
+                text: turnContext._activity.text,
+                context: this.contexto
             })
                 .then(async (res) => {
-                    console.log(res.data.output);
                     await turnContext.sendActivity(res.data.output.generic[0].text);
+                    console.log(res.data.output);
+                    this.contexto = res.data.context;
                 })
                 .catch((err) => {
                     console.log(err);
